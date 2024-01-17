796cdcfe65a69d1f63e6328efc13c77e80ffb69c
Geraldine Gómez <geralgomez1403@gmail.com>
2024-01-17 17:35:00 -0500

    angular forms lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-forms';
+  formGroup: FormGroup
+
+  constructor(private _builder: FormBuilder){
+    this.formGroup = this._builder.group({
+      user: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(form: any){
+    alert(`User: ${form.user}\nPassword: ${form.password}`)
+  }
 }
